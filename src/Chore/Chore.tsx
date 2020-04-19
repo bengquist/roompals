9e1beb76bc29df22fa7e4b6e12082700879e5da3
bengquist <engquist_blake@yahoo.com>
2020-04-18 21:12:50 -0500

    implement calendar

--- src/Chore/Chore.tsx
+++ src/Chore/Chore.tsx
@@ -1,14 +1,28 @@
 import React from 'react';
-import {Text} from 'react-native';
-import Center from 'src/Style/Center';
+import {Text, View} from 'react-native';
+import {Calendar} from 'react-native-calendars';
 
 interface ChoreProps {}
 
 const Chore: React.FC<ChoreProps> = ({}) => {
   return (
-    <Center>
+    <View>
+      <Calendar
+        current={'2020-04-18'}
+        onDayPress={(day) => {
+          console.log('selected day', day);
+        }}
+        onDayLongPress={(day) => {
+          console.log('selected day', day);
+        }}
+        monthFormat={'MMMM yyyy'}
+        onMonthChange={(month) => {
+          console.log('month changed', month);
+        }}
+        disableMonthChange={true}
+      />
       <Text>Chore</Text>
-    </Center>
+    </View>
   );
 };
 

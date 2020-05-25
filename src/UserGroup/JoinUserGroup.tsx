10f56ce0b40f6036b7880cd979fa659defe2f6f2
bengquist <engquist_blake@yahoo.com>
2020-05-24 23:32:16 -0500

    okay

--- src/UserGroup/JoinUserGroup.tsx
+++ src/UserGroup/JoinUserGroup.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Text} from 'react-native';
+import {useJoinUserGroupMutation, useMeQuery} from 'src/generated/graphql';
 import Button from 'src/Style/Button';
 import Center from 'src/Style/Center';
 import Input from 'src/Style/Input';
@@ -9,11 +10,29 @@ import {UserGroupProps} from './types';
 const JoinUserGroup: React.FC<UserGroupProps<'JoinUserGroup'>> = ({
   navigation,
 }) => {
+  const [groupName, setGroupName] = useState('');
+  const {data: meData} = useMeQuery();
+  const [joinUserGroup] = useJoinUserGroupMutation();
+
+  const joinGroupHandler = async () => {
+    if (groupName && meData?.me.id) {
+      const what = await joinUserGroup({
+        variables: {groupName, userId: meData?.me.id},
+      });
+
+      console.log(what.data?.joinUserGroup.id);
+    }
+  };
+
   return (
     <Center flex={1}>
       <Padding>
-        <Input placeholder="Group name" />
-        <Button>Join Group</Button>
+        <Input
+          value={groupName}
+          onChangeText={(value) => setGroupName(value)}
+          placeholder="Group name"
+        />
+        <Button onPress={joinGroupHandler}>Join Group</Button>
         <Text>or</Text>
         <Button onPress={() => navigation.navigate('CreateUserGroup')}>
           Create Group
